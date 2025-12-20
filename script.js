e14357e26ee227134c764e3bf774881b4b9a12c3
David Laurence Aviado <davidaviado.dla@gmail.com>
2025-12-20 22:21:40 +0800

    Feat: Add error handling and input validation

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 function createGrid(size) {
-  console.log(`Creating ${size}x${size} grid...`);
+  console.log(`Creating ${size}x${size} grid`);
   const container = document.getElementById("grid");
 
   container.innerHTML = "";
+  console.log("Old grid cleared");
 
   container.style.gridTemplateColumns = `repeat(${size}, 50px)`;
-  console.log(`Grid columns set to: repeat(${size}, 50 px)`);
+  console.log(`Grid colums set to: repeat(${size}, 50px)`);
 
   const totalCells = size * size;
+  console.log(`Total cells to create: ${totalCells}`);
 
-  for (let i = 0; i < totalCells; i++);
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  container.appendChild(cell);
+  for (let i = 0; i < totalCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
+  console.log("Grid creation complete!");
 }
 
 const userInput = prompt("Enter grid size (1-100):");
 
-console.log("User entered:", userInput);
-console.log("Type of input:", typeof userInput);
-
-const gridSize = parseInt(userInput);
-console.log("Converted to a number:", gridSize);
-console.log("Type after conversion:", typeof gridSize);
-
-if (gridSize >= 1 && gridSize <= 100) {
-  console.log("Valid input, creating grid...");
-  createGrid(gridSize);
+if (userInput === null) {
+  console.log("User cancelled");
+  alert("So you don't want to make a grid?");
 } else {
-  console.log("Invalid input!");
-  alert("Please enter a number between 1 and 100 only.");
+  console.log("User entered:", userInput);
+  console.log("Type of input:", typeof userInput);
+
+  const gridSize = parseInt(userInput);
+  console.log("Converted to a number:", gridSize);
+  console.log("Type after conversion:", typeof gridSize);
+
+  if (isnan(gridSize)) {
+    console.log("🔴 Not a number!");
+    alert("Hey! That's not a number! Please enter a valid number.");
+  } else if (gridSize < 1 || gridSize > 100) {
+    console.log("🔴 Out of range!");
+    alert("That's out of range! Just pick a number 1-100.");
+  } else {
+    console.log("🟢Valid input, creating grid...");
+    createGrid(gridSize);
+  }
 }
